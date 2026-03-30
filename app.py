466baf8f3e23bc1d2883ae67d84e418ff26f0cc7
Annisadpk <127417498+Annisadpk@users.noreply.github.com>
2026-03-30 14:23:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -327,9 +327,12 @@ def plot_customer_distribution_city():
     data = run_query(query) 
     world = gpd.read_file(
     "https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_110m_admin_0_countries.geojson")
-    merged_data = world.merge(data, how='left', left_on='name', right_on='City') 
-    fig, ax = plt.subplots(figsize=(10, 6)) 
-    merged_data.plot(column='CustomerCount', cmap='Blues', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True) 
+     # merge, ganti 'name' jadi 'admin'
+    merged_data = world.merge(data, how='left', left_on='admin', right_on='City')
+    
+    fig, ax = plt.subplots(figsize=(10, 6))
+    merged_data.plot(column='CustomerCount', cmap='Blues', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+    
     plt.title('Distribusi Pelanggan Berdasarkan Kota') 
     plt.xlabel('Longitude') 
     plt.ylabel('Latitude') 
